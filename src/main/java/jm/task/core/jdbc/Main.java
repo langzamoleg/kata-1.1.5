aac1fe86ed9e031d7ba350f822c0c6bf81159e29
Oleg Zamyatin <langzamoleg@yandex.ru>
2023-01-11 17:21:18 +0300

    aaa

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,46 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static jm.task.core.jdbc.util.Util.connection;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+
+        UserService service = new UserServiceImpl();
+
+        service.createUsersTable();
+
+        User user1 = new User("Konstantin", "Voronin", (byte) 40);
+        service.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+        System.out.println("User " + user1.getName() + " added");
+
+        User user2 = new User("Vera", "Voronina", (byte) 35);
+        service.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        System.out.println("User " + user2.getName() + " added");
+
+        User user3 = new User("Leonid", "Voronin", (byte) 45);
+        service.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        System.out.println("User " + user3.getName() + " added");
+
+        User user4 = new User("Galina", "Voronina", (byte) 75);
+        service.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+        System.out.println("User " + user4.getName() + " added");
+
+        List<User> out = service.getAllUsers();
+        for (int i = 0; i < out.size(); i++) {
+            User user_out = out.get(i);
+            System.out.println(user_out.getName() + user_out.getLastName() + user_out.getAge());
+        }
+
+        service.cleanUsersTable();
+        service.dropUsersTable();
+
+        connection.close();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,74 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    public static Util util = new Util();
+    public static Connection connection = util.connection();
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        try (Statement preparedStatement = connection.createStatement()) {
+            preparedStatement.executeUpdate("CREATE TABLE IF NOT EXISTS user_for_task (id INT PRIMARY KEY AUTO_INCREMENT,name VARCHAR(30), lastName VARCHAR(35), age INT);");
+        } catch (SQLException ignored) {
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Statement preparedStatement = connection.createStatement()) {
+            preparedStatement.executeUpdate("DROP TABLE user_for_task;");
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Statement preparedStatement = connection.createStatement()) {
+            preparedStatement.executeUpdate("INSERT INTO user_for_task (name, lastName, age) VALUES ('" + name + "', '" + lastName + "', " + age + ");");
+        } catch (SQLException throwable) {
+            throwable.getStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (Statement preparedStatement = connection.createStatement()) {
+            preparedStatement.executeUpdate("DELETE FROM user_for_task WHERE id = " + id + ";");
+        } catch (SQLException throwable) {
+            throwable.getStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> result = new ArrayList<>();
+        try (Statement preparedStatement = connection.createStatement()) {
+            ResultSet list = preparedStatement.executeQuery("SELECT name, lastName, age FROM user_for_task;");
+            while (list.next()) {
+                User user_new = new User(list.getString("name"),
+                        list.getString("lastName"), list.getByte("age"));
+                result.add(user_new);
+            }
+        } catch (SQLException throwable) {
+            throwable.getStackTrace();
+        }
+        return result;
     }
 
     public void cleanUsersTable() {
-
+        try (Statement preparedStatement = connection.createStatement()) {
+            preparedStatement.executeUpdate("TRUNCATE user_for_task;");
+        } catch (SQLException ignored) {
+        }
     }
 }
