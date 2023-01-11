110a71c8f726a5dcb208273d04dea2d9ee7007c3
Oleg Zamyatin <langzamoleg@yandex.ru>
2023-01-11 17:28:40 +0300

    aaa

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package jm.task.core.jdbc.service;
 
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
@@ -9,7 +8,6 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
     private UserDaoJDBCImpl daoJDBC = new UserDaoJDBCImpl();
-    private UserDaoHibernateImpl daoHibernate = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
         daoJDBC.createUsersTable();
